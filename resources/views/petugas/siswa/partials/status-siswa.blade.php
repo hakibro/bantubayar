ff2912fa3045ff018bf4990927ddb301db854439
hakibro <sandalkuganti@gmail.com>
2026-01-18 11:22:57 +0700

    fix status lunas/belum lunas/ belum sinkron

--- resources/views/petugas/siswa/partials/status-siswa.blade.php
+++ resources/views/petugas/siswa/partials/status-siswa.blade.php
@@ -1,11 +1,17 @@
     <div class="whitespace-nowrap">
-        @if (is_null($item->getKategoriBelumLunas()))
-            <span class="text-xs bg-yellow-600 text-white px-3 py-1 rounded-full font-semibold">Belum
-                Sinkron</span>
-        @elseif ($belumLunas < 0)
-            <span class="text-xs bg-red-600 text-white px-3 py-1 rounded-full font-semibold">Belum
-                Lunas</span>
-        @else
-            <span class="text-xs bg-green-600 text-white px-3 py-1 rounded-full font-semibold">Lunas</span>
-        @endif
+        <span
+            class="{{ $item->getTotalTunggakan() < 0
+                ? 'bg-accent'
+                : ($item->getKategoriBelumLunas() === null
+                    ? 'bg-yellow-600'
+                    : 'bg-success') }} text-xs text-white px-3 py-1 rounded-full font-semibold">
+            @if (is_null($item->getKategoriBelumLunas()))
+                Belum Sinkron
+            @elseif ($item->getTotalTunggakan() < 0)
+                Belum Lunas
+            @else
+                Lunas
+            @endif
+        </span>
+
     </div>
